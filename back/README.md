0b372a66f37533c9e0b4ee23ac4595faa46ee7ab
Fabricio Tello <ftello@puntoticket.cl>
2020-05-10 13:09:12 -0400

    Modificiación en server.py y requirements.txt. Incorpora readme para backend. Elimina archivo MakeFile.win

---
+++ back/README.md
@@ -0,0 +1,5 @@
+# Ejecución de backend
+- Ir a ruta donde se encuentra el archivo server.py, ejemplo: "E:\personal\test-programacion\back"
+- Abrir una consola, escribir esta línea:
+    - pip install --upgrade -r requirements.txt . Con esto se instalan las dependencias a utilizar en proyecto.
+    - server.py runserver . Con esto levantamos el servidor de python.
--- back/server.py
+++ back/server.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+URL_ULTIMOS_INDICADORES = 'https://www.indecon.online/last'
+URL_POR_TIPO_INDICADOR = 'https://www.indecon.online/values/{}'
+URL_POR_FECHA_E_INDICADOR = 'https://www.indecon.online/date/{}/{}'
 
+@app.route("/", methods=['GET'])
 def obtenerUltimosIndicadores():
-    return "Hola somos los indicadores"
+    respuesta = requests.get(url=URL_ULTIMOS_INDICADORES)
+    if respuesta.status_code != 200:
+        return "Error en obtener últimos indicadores" , 500
+    return respuesta.json()
+
+
+@app.route("/indicador/<nombre_indicador>", methods=['GET'])
+def obtenerIndicador(nombre_indicador):
+    if request.args.get('fecha'):
+        print("tiene fecha")
+        respuesta = requests.get(url=URL_POR_FECHA_E_INDICADOR.format(nombre_indicador, request.args.get('fecha')))
+    else:
+        respuesta = requests.get(url=URL_POR_TIPO_INDICADOR.format(nombre_indicador))
+
+    if respuesta.status_code != 200:
+        return "Error en obtener indicador seleccionado", 500
+    if not respuesta.json():
+        return "No existe información para el indicador seleccionado", 500
+    return respuesta.json()
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000)
