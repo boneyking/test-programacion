82df5ef093d9d2d428be488929a7613239704b86
Fabricio Tello <ftello@puntoticket.cl>
2020-05-10 12:13:22 -0400

    Primer commit

---
+++ back/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+
+def obtenerUltimosIndicadores():
+    return "Hola somos los indicadores"
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000)
