c5d5a301275af7354bef732e4e8f761a29868d6a
Fabricio Tello <ftello@puntoticket.cl>
2020-05-11 18:18:29 -0400

    Modificaciones visuales

--- front/indicadores/src/componentes/por-indicador/por-indicador.js
+++ front/indicadores/src/componentes/por-indicador/por-indicador.js
@@ -1,15 +1,66 @@
 import React from "react";
+import Moment from 'moment';
+import { Chart } from 'react-charts';
 
 export class PorIndicador extends React.Component {
-    // llaveIndicador = '';
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            llave: '',
+            nombre: '',
+            unidad: '',
+            valores: []            
+        };
+    }
+
+    obtenerIndicadorPorTipo() {
+        Moment.locale('es');
+        fetch('http://localhost:5000/api/indicador/cobre')
+            .then(response => response.json())
+            .then(indicadores => {
+                let listadoValores = [];
+                Object.keys(indicadores.values).forEach(fecha => {
+                    listadoValores.push({
+                        fecha: Moment(fecha).format('DD-MM-yyyy'),
+                        valor: indicadores.values[fecha]
+                    });
+                });
+
+                this.setState({
+                    llave: indicadores.key,
+                    nombre: indicadores.name,
+                    unidad: indicadores.unit,
+                    valores: listadoValores
+                });
+            },
+            () => {
+                this.setState({
+                    llave: '',
+                    nombre: '',
+                    unidad: '',
+                    valores: [] 
+                });
+            });
+    }
+
+
 
     render() {
+
         if(this.props.llaveIndicador === ''){
             return (<div className="col-sm-12 col-lg-4 col-xl-4"></div>);
         } else {
+            this.obtenerIndicadorPorTipo();
             return (
                 <div className="col-sm-12 col-lg-4 col-xl-4">
                     <h1>{this.props.llaveIndicador}</h1>
+                    <>
+                    
+                    
+                                                  
+                    </>
+                    
                 </div>
             );
         }        
--- front/indicadores/src/componentes/ultimos-indicadores/ultimos-indicadores.js
+++ front/indicadores/src/componentes/ultimos-indicadores/ultimos-indicadores.js
@@ -12,11 +12,19 @@ class UltimosIndicadores extends React.Component {
             seCargaronIndicadores: false,
             mensaje: '',
             indicadores: [],
-            llave: ''
+            llave: '',
+            paginaActual: 1,
+            cantidadPorPagina: 2
         };
+        this.handleAvanzar = this.handleAvanzar.bind(this);
+
     }
 
-
+    handleAvanzar(event) {
+        this.setState({
+            paginaActual: Number(event.target.id)
+        });
+    }
 
     tipoUnidad(tipo, valor){
         let tipoMoneda = <CurrencyFormat value={valor} displayType={'text'} thousandSeparator={true} prefix={'$ '} />;
@@ -53,12 +61,22 @@ class UltimosIndicadores extends React.Component {
                     });
                 });
 
-                this.setState({
-                    seCargaronIndicadores: true,
-                    mensaje: 'Se cargaron Indicadores',
-                    indicadores: listadoIndicadores,
-                    llave: ''
-                });
+                if (listadoIndicadores.length > 0) {
+                    this.setState({
+                        seCargaronIndicadores: true,
+                        mensaje: 'Se cargaron Indicadores',
+                        indicadores: listadoIndicadores,
+                        llave: ''
+                    });
+                } else {
+                    this.setState({
+                        seCargaronIndicadores: true,
+                        mensaje: 'No hay Indicadores',
+                        indicadores: [],
+                        llave: ''
+                    });
+                }
+                
             },
             () => {
                 this.setState({
@@ -73,32 +91,112 @@ class UltimosIndicadores extends React.Component {
     render() {
         const {handlePorIndicador} = this.props;
 
+
         if (this.state.seCargaronIndicadores) {
-            return (
-                <div className="col-sm-12 col-lg-4 col-xl-4">
-                    <ul className="list-group">
-                        <li className="list-group-item d-flex justify-content-between align-items-center"><h3>Últimos Indicadores</h3></li>
-                        {this.state.indicadores.map(indicador => 
-                            <li key={indicador.llave} className="list-group-item d-flex justify-content-between align-items-center">
-                                <div className="card ancho-maximo">
-                                    <h5 className="card-header">{indicador.nombre.toUpperCase()} <span className="float-right">{Moment(indicador.fecha).format('DD-MM-yyyy')}</span> </h5>
-                                    <div className="card-body">
-                                        <h6 className="card-title">{indicador.descripcion}</h6>
-                                        <p className="card-text">
-                                            {this.tipoUnidad(indicador.unidad, indicador.valor)}
-                                        </p>
-                                        <button type="button" className="btn btn-primary" 
-                                            onClick={() => handlePorIndicador(indicador.llave)}>Go somewhere</button>
-                                    </div>
-                                </div>
+            if (this.state.indicadores.length > 0) {
+                const indicePagina = this.state.paginaActual * this.state.cantidadPorPagina;
+                const indiceInicial = indicePagina - this.state.cantidadPorPagina;
+                const todos = this.state.indicadores.slice(indiceInicial, indicePagina);
+        
+                const renderTodos = todos.map((indicador, index) => {
+                    return <li key={indicador.llave} className="list-group-item d-flex justify-content-between align-items-center">
+                    <div className="card ancho-maximo">
+                        <h5 className="card-header">{indicador.nombre.toUpperCase()} <span className="float-right">{Moment(indicador.fecha).format('DD-MM-yyyy')}</span> </h5>
+                        <div className="card-body">
+                            <h6 className="card-title">{indicador.descripcion}</h6>
+                            <p className="card-text">
+                                {this.tipoUnidad(indicador.unidad, indicador.valor)}
+                            </p>
+                            <button type="button" className="btn btn-primary" 
+                                onClick={() => handlePorIndicador(indicador.llave)}>Go somewhere</button>
+                        </div>
+                    </div>
+                </li>
+                });
+        
+                const numerosPagina = [];
+                for (let i = 1; i <= Math.ceil(this.state.indicadores.length / this.state.cantidadPorPagina); i++){
+                    numerosPagina.push(i);
+                }
+    
+                const renderNumerosPaginas = numerosPagina.map(numero => {
+                    return (
+                        <>
+                        {
+                            this.state.paginaActual === numero &&
+                            <li
+                                key={numero}
+                                id={numero}
+                                onClick={this.handleAvanzar}
+                                className="pagina-activa"
+                            >
+                                {numero}
                             </li>
-                        )}
-                    </ul>
-                </div>
-            );
-        } else {
-            return 'Cargando últimos indicadores';
-        }        
+                        }
+    
+                        {
+                            this.state.paginaActual !== numero &&
+                            <li
+                                key={numero}
+                                id={numero}
+                                onClick={this.handleAvanzar}
+                                className="pagina-inactiva"
+                            >
+                                {numero}
+                            </li>
+                        }
+                        </>
+                        
+                    );
+                });
+        
+                return (
+                    <div className="col-sm-12 col-lg-4 col-xl-4">
+                        <ul className="list-group">
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <h3>Últimos Indicadores</h3>
+                                <span className="badge badge-pill badge-primary">{this.state.paginaActual}</span>
+                            </li>
+                            {renderTodos}
+                        </ul>
+                        <ul id="page-numbers">
+                            {renderNumerosPaginas}
+                        </ul>
+                    </div>
+                );
+            }
+            return this.state.mensaje;
+            
+        }
+        return 'Cargando últimos indicadores';
+        
+
+        // if (this.state.seCargaronIndicadores) {
+        //     return (
+        //         <div className="col-sm-12 col-lg-4 col-xl-4">
+        //             <ul className="list-group">
+        //                 <li className="list-group-item d-flex justify-content-between align-items-center"><h3>Últimos Indicadores</h3></li>
+        //                 {this.state.indicadores.map(indicador => 
+        //                     <li key={indicador.llave} className="list-group-item d-flex justify-content-between align-items-center">
+        //                         <div className="card ancho-maximo">
+        //                             <h5 className="card-header">{indicador.nombre.toUpperCase()} <span className="float-right">{Moment(indicador.fecha).format('DD-MM-yyyy')}</span> </h5>
+        //                             <div className="card-body">
+        //                                 <h6 className="card-title">{indicador.descripcion}</h6>
+        //                                 <p className="card-text">
+        //                                     {this.tipoUnidad(indicador.unidad, indicador.valor)}
+        //                                 </p>
+        //                                 <button type="button" className="btn btn-primary" 
+        //                                     onClick={() => handlePorIndicador(indicador.llave)}>Go somewhere</button>
+        //                             </div>
+        //                         </div>
+        //                     </li>
+        //                 )}
+        //             </ul>
+        //         </div>
+        //     );
+        // } else {
+        //     return 'Cargando últimos indicadores';
+        // }        
     }
 }
 
