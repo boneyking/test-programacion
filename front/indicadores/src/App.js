3305fe6f0d347fb584ef759424008b30053c4ccf
Fabricio Tello <ftello@puntoticket.cl>
2020-05-13 11:13:08 -0400

    Remueve componentes creados anteriormente y se deja solo 1 componente que realiza todas las consultas

--- front/indicadores/src/App.js
+++ front/indicadores/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <h1> Indicadores </h1>
+        <div className="col-12 text-center">
+          <h1> Indicadores </h1>
+        </div>        
       </div>
       <div className="row">
         <div className="container">
